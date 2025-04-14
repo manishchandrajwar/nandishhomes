3ca852c24b6f71eddac06f342cc2f64c5461ba63
manishchandrajwar <contactmanish05@gmail.com>
2025-04-14 14:26:25 +0530

    New pictures added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,11 @@ import image1 from "../assets/img1.jpeg";
 import image2 from "../assets/img2.jpeg";
 import image3 from "../assets/img3.jpeg";
 import image4 from "../assets/img4.jpeg";
+import image5 from "../assets/img5.jpeg";
+import image6 from "../assets/img6.jpeg";
+import image7 from "../assets/img7.jpeg";
+import image8 from "../assets/img8.jpeg";
+
 
 const Hero = () => {
   return (
@@ -13,6 +18,10 @@ const Hero = () => {
           <img src={image2} alt="Slide 2" />
           <img src={image3} alt="Slide 3" />
           <img src={image4} alt="Slide 4" />
+          <img src={image5} alt="Slide 5" />
+          <img src={image6} alt="Slide 6" />
+          <img src={image7} alt="Slide 7" />
+          <img src={image8} alt="Slide 8" />
         </div>
       </div>
       <div className="overlay">
