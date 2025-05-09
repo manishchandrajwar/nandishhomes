0736b2070d776a296ea81232efe24d448a26f398
manishchandrajwar <contactmanish05@gmail.com>
2025-05-10 00:32:39 +0530

    Added

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -5,7 +5,7 @@ function AboutUs() {
   return (
     <section id="about" className="about-section">
       <h2>About Us</h2>
-      <p>Welcome to Nandish Homes – your luxurious stay in Pondicherry.</p>
+      <p>Welcome to Nandish Homes – manish your luxurious stay in Pondicherry.</p>
     </section>
   );
 }
