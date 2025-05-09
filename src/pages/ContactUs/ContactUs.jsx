98e90a10dd528675561ba9257a5874cabf302a3e
manishchandrajwar <contactmanish05@gmail.com>
2025-05-10 00:17:56 +0530

    added new things

---
+++ src/pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ContactUs.css';
+
+function ContactUs() {
+  return (
+    <section id="contact" className="contact-section">
+      <h2>Contact Us</h2>
+      <p>Get in touch to book your stay at Nandish Homes.</p>
+    </section>
+  );
+}
+
+export default ContactUs;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <section className="home-section">
+      <h2>Welcome to Nandish Homes</h2>
+      <p>Experience a lavish stay in Pondicherry.</p>
+    </section>
+  );
+}
+
+export default Home;
