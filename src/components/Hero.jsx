c9a4f3f7ba8cde386cf758d31a7fa591c84a0840
manishchandrajwar <contactmanish05@gmail.com>
2025-05-10 00:17:41 +0530

    total change new code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
         </div>
       </div>
       <div className="overlay">
-        <h1>Book Your Dream Villa</h1>
+        <h1>Book Your Dream Villa Today</h1>
       </div>
     </div>
   );
