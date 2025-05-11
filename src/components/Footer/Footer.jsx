edb0d95c1fa2f444fab4a113904d3392877cfc55
manishchandrajwar <contactmanish05@gmail.com>
2025-05-11 11:24:28 +0530

    New changes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import "./Footer.css";
+import { FaInstagram, FaFacebookF } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-socials">
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebookF />
+        </a>
+      </div>
+      <div className="footer-contact">
+        <p>
+          Address: RS No 181/3A3, R Farm Lands, Pattanur Village, Vanur Taluk, Tamil Nadu 605111
+        </p>
+        <p>Phone: 09628298651</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
